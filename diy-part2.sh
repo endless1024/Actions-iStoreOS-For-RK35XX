3ddb3a01ed7f26d6752ee0c090a3c287a07f07cf
endless1024 <327226555@qq.com>
2024-09-26 15:25:02 +0800

    rk3568-nsy-g68-plus1

--- diy-part2.sh
+++ diy-part2.sh
@@ -83,7 +83,7 @@ echo -e "\\ndefine Device/nsy_g68-plus
 \$(call Device/rk3568)
   DEVICE_VENDOR := NSY
   DEVICE_MODEL := G68PLUS
-  DEVICE_DTS := rk3568-nsy-g68-plus
+  DEVICE_DTS := rk3568-nsy-g68-plus1
   SUPPORTED_DEVICES += nsy,g68-plus
   DEVICE_PACKAGES := kmod-nvme kmod-scsi-core kmod-thermal kmod-switch-rtl8306 kmod-switch-rtl8366-smi kmod-switch-rtl8366rb kmod-switch-rtl8366s kmod-hwmon-pwmfan kmod-leds-pwm kmod-r8125 kmod-r8168 kmod-switch-rtl8367b swconfig
 endef
@@ -115,8 +115,8 @@ sed -i "s/inspur,ihec301|\\\/&\\n	nsy,g68-plus|\\\/g" target/linux/rockchip/rk35
 
 cp -f $GITHUB_WORKSPACE/configfiles/rk3568-ynh518.dts target/linux/rockchip/dts/rk3568/rk3568-ynh518.dts
 
-cp -f $GITHUB_WORKSPACE/configfiles/rk3568-nsy-g68-plus.dts target/linux/rockchip/dts/rk3568/rk3568-nsy-g68-plus.dts
-cp -f $GITHUB_WORKSPACE/configfiles/rk3568-nsy-g68-plus-core.dtsi target/linux/rockchip/dts/rk3568/rk3568-nsy-g68-plus-core.dtsi
+cp -f $GITHUB_WORKSPACE/configfiles/rk3568-nsy-g68-plus1.dts target/linux/rockchip/dts/rk3568/rk3568-nsy-g68-plus1.dts
+cp -f $GITHUB_WORKSPACE/configfiles/rk3568-nsy-g68-plus1.dtb target/linux/rockchip/dts/rk3568/rk3568-nsy-g68-plus1.dtb
 
 
 cp -f $GITHUB_WORKSPACE/configfiles/rk3568-firefly-roc-pc-se-core.dtsi target/linux/rockchip/dts/rk3568/rk3568-firefly-roc-pc-se-core.dtsi
